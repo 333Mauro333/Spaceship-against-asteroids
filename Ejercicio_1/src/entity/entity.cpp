7bc1640f25d8d1a193cb5c9da7ab33e6b6578aa1
Mauro Guillermo Torres Velasquez <maurotv50@gmail.com>
2021-12-12 02:30:43 -0300

    Armado de la clase de entidad

--- Ejercicio_1/src/entity/entity.cpp
+++ Ejercicio_1/src/entity/entity.cpp
@@ -1,3 +1,33 @@
 #include "entity.h"
 
+#include <iostream>
 
+#include "AwesomeLibrary.h"
+
+using std::cout;
+
+
+Entity::Entity(int x, int y, int w, int h)
+{
+	position = { x, y };
+	size = { w, h };
+
+	setForegroundColor(Color::GREEN);
+	cout << "Se ha creado una entidad.\n";
+	setForegroundColor(Color::WHITE);
+}
+Entity::~Entity()
+{
+	setForegroundColor(Color::RED);
+	cout << "La entidad ha sido eliminada de la memoria.\n\n";
+	setForegroundColor(Color::WHITE);
+}
+
+Vector2i Entity::getPosition()
+{
+	return position;
+}
+bool Entity::collidesWith(Entity* entity)
+{
+	return position.x == entity->position.x && position.y == entity->position.y;
+}
--- Ejercicio_1/src/entity/entity.h
+++ Ejercicio_1/src/entity/entity.h
@@ -2,14 +2,31 @@
 #define ENTITY_H
 
 
+struct Vector2i
+{
+	int x;
+	int y;
+};
+
 class Entity
 {
 public:
-	Entity();
+	Entity(int x, int y, int w, int h);
 	~Entity();
 
-private:
+	virtual void update() = 0;
+	virtual void draw() = 0;
 
+	Vector2i getPosition();
+	virtual bool collidesWith(Entity* entity);
+
+protected:
+	Vector2i position;
+
+	Vector2i getSize();
+
+private:
+	Vector2i size;
 };
 
 #endif // !ENTITY_H
